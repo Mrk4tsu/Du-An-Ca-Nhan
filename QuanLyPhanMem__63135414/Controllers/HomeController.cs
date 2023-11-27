5b819bc5df878ccb5798a6d2b39aba2751583181
Nguyễn Đức Thắng <thang.ndu.63cntt@ntu.edu.vn>
2023-11-28 01:52:20 +0700

    Update Login, Logout

--- QuanLyPhanMem__63135414/Controllers/HomeController.cs
+++ QuanLyPhanMem__63135414/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace QuanLyPhanMem__63135414.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public ActionResult Index()
         {
             return View();
